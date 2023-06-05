38bc8963238446ada7787e23d3b43fe658fa7398
tysonle7 <125998611+tysonle7@users.noreply.github.com>
2023-06-05 16:14:00 -0500

    Add files via upload

---
+++ mathandgreet.py
@@ -0,0 +1,12 @@
+print("Hello")
+name = input("What is your name?")
+print("Hello" + " " + name)
+
+value1 = int(input(What is value one?))
+value2 = int(input(what is value two?))
+
+totalvalue = value1 * value2
+
+endmessage = ("Hello" + name + "the answer to your math question: ")
+print(endMessage)
+print(totalvalue)
